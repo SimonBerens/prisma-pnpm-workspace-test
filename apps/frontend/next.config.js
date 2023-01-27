02a990425354eb7f1912e1cd42be6b3cc2a10c9e
pierre <pierreantoine.urvoy@gmail.com>
2023-01-26 15:59:39 -1000

    workaround

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,6 +1,19 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (isServer) {
+      config.externals = [...config.externals, '@prisma/client']
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
